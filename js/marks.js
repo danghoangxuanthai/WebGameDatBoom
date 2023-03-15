c73744daa160ff7a344721e7f4a58c0a139c2779
nguyentanthai <nguyentanthai2017@gmail.com>
2023-03-15 23:24:58 +0700

    commit

--- js/marks.js
+++ js/marks.js
@@ -3,7 +3,11 @@ class Marks{
     this.player_blue = 'player1';
     this.player_pink = 'player2';
     this.bomb = 'bomb';
-    
+     // General field
+     this.empty = 'empty';
+     this.destructive = 'destructive';
+     this.undestructive = 'undestructive';
+     this.fire = 'fire';
 
   }
 }
