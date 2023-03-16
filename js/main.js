0e1677cb85215a2d18751f54e4f04d38216d3cfd
nguyentanthai <nguyentanthai2017@gmail.com>
2023-03-16 22:47:20 +0700

    commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var fieldHeight = 12;
+var fieldWidth = 17;
+
+loadImage("img/tileset.png")
+.then(image => {
+  var drawer = new Drawer(fieldHeight, fieldWidth, image);
+  var game = new Game(drawer, fieldHeight, fieldWidth);
+});
