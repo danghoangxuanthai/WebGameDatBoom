09e734d64d093db98879249863cf1162f4005fcc
nguyentanthai <nguyentanthai2017@gmail.com>
2023-03-17 20:30:02 +0700

    tạo class GeneralField

---
+++ js/general_field.js
@@ -0,0 +1,14 @@
+class GeneralField{
+    constructor(h, w){
+        this.height = h;
+        this.width = w;
+        this.field = this._generateField();
+        this.bonuses = this._generateBonuses();
+        while (!this._fieldIsValid(this.field)){
+          console.log('Regenerate');
+          this.field = this._generateField();
+        }
+      }
+
+
+}
