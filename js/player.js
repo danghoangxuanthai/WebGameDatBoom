aa8a7d14b26de4b3d7794bb48da4a8ced1cda019
Thái <thaid373@gmail.com>
2023-03-17 23:50:15 +0700

    first commit

--- js/player.js
+++ js/player.js
@@ -28,5 +28,36 @@ class Player{
         if ((direction == 'right') && (general_field.field[this.actualY()][Math.ceil(this.x)] == marks.empty)) this.x += this.speed
         else if ((direction == 'left') && (general_field.field[this.actualY()][Math.floor(this.x)] == marks.empty)) this.x -= this.speed;
       }
+      if (general_field.bonuses[this.actualY()][this.actualX()] == marks.bonus_bomb_plus){
+        this._bonus_bomb_plus();
+        general_field.bonuses[this.actualY()][this.actualX()] = null;
+      }
+  
+      if (general_field.bonuses[this.actualY()][this.actualX()] == marks.bonus_speed_plus){
+        this._bonus_speed_plus();
+        general_field.bonuses[this.actualY()][this.actualX()] = null;
+      }
     }
+    placeBomb(){
+      return new Bomb(this.actualX(), this.actualY(), this.bombPower);
+    }
+  
+    _bonus_speed_plus(){
+      this.speed = Math.min(0.5, this.speed + 0.02);
+      this._display_speed();
+    }
+  
+    _bonus_bomb_plus(){
+      this.bombPower = Math.min(5, this.bombPower + 1);
+      this._display_bomb_power();
+    }
+  
+    _display_speed(){
+      document.getElementById(this.name + "_speed").innerHTML = Math.round(this.speed * 100) / 100;
+    }
+  
+    _display_bomb_power(){
+      document.getElementById(this.name + "_bomb-power").innerHTML = Math.round(this.bombPower * 100) / 100;
+    }
+    
 }
