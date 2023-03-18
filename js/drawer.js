9661cf2511ffd31706e89afcb119047287b15419
Thái <thaid373@gmail.com>
2023-03-18 23:55:52 +0700

    commit 2

--- js/drawer.js
+++ js/drawer.js
@@ -6,4 +6,32 @@ class Drawer{
       this.context.font = '48px serif';
       this._defineTiles(image);
     }
+
+    _defineTiles(image){
+      this.sprites = new SpriteSheet(image);
+        this.sprites.defineTile(marks.destructive, 2, 0);
+        this.sprites.defineTile(marks.empty, 0, 0);
+        this.sprites.defineTile(marks.undestructive, 1, 0);
+        this.sprites.defineTile(marks.player_blue, 0, 2);
+        this.sprites.defineTile(marks.player_pink, 2, 2);
+        this.sprites.defineTile(marks.bomb, 0, 1);
+        this.sprites.defineTile(marks.fire, 2, 1);
+        this.sprites.defineTile(marks.bonus_bomb_plus, 1, 1);
+        this.sprites.defineTile(marks.bonus_speed_plus, 1, 2);
+    }
+  
+    drawField(general_field){
+      for (let y = 0; y < general_field.height; y++){
+        for (let x = 0; x < general_field.width; x++){
+          this.sprites.drawTile(general_field.field[y][x], this.context, x, y);
+        }
+      }
+      for (let y = 0; y < general_field.height; y++){
+        for (let x = 0; x < general_field.width; x++){
+          if ((general_field.bonuses[y][x] != null) &&
+          (general_field.field[y][x] == marks.empty))
+          this.sprites.drawTile(general_field.bonuses[y][x], this.context, x, y);
+        }
+      }
+    }
 }  
