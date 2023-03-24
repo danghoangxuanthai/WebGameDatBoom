7700a910c9a799e7f52585e14e0c1f34591f563b
MinhTam6348 <ngominhtam1210@gmail.com>
2023-03-24 18:40:10 +0700

    add more sprite_sheet

--- js/sprite_sheet.js
+++ js/sprite_sheet.js
@@ -24,4 +24,22 @@ class SpriteSheet {
                 this.height);
         this.tiles.set(name, buffer);
     }
+
+    defineTile(name, x, y) {
+        this.define(
+            name,
+            x * this.width,
+            y * this.height,
+            this.width,
+            this.height);
+        }
+    
+        draw(name, context, x, y) {
+            const buffer = this.tiles.get(name);
+            context.drawImage(buffer, x, y);
+        }
+    
+        drawTile(name, context, x, y) {
+            this.draw(name, context, x * this.width, y * this.height);
+        }
 }
