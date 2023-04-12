a052395fbd9fa60ae7c9c6b73d903d221bedb22f
MinhTam6348 <ngominhtam1210@gmail.com>
2023-04-12 19:22:24 +0700

    player

--- js/image_loader.js
+++
@@ -1,9 +0,0 @@
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
-}
